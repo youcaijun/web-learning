f743437b65d877a66621d562f2bd677159dfdbc8
USER-20230617ZO\Administrator <872137092@qq.com>
2025-08-19 23:56:05 +0800

    新增环境部署

---
+++ my-first-app/src/views/v-show.vue
@@ -0,0 +1,12 @@
+<!--v-show demo -->
+<script setup>
+// import { ref } from 'vue';
+// // 定义变量 name   
+// var name= ref("");
+</script>
+
+ <!--v-model与插值绑定-->
+<template>
+    <button v-show="true">显示</button>
+    <button v-show="false">隐藏</button>
+</template>
